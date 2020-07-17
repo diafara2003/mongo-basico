8f180acabc50ae91016c1c99b9ebf7caa7a95eb0
Jhonnatan andres <jaud_@hotmail.com>
2020-07-17 13:51:08 -0500

    :mongo crud

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const db = require('./DataBase/Connection');
 const app = express();
 const port = process.env.Port || 3000;
 
+
+app.use(cors());
 app.use(bodyParser.json());
+
+db();
+
 
 app.get('/',(req,res)=>{
     res.send("we are on home")
@@ -15,7 +21,7 @@ app.use('/posts',(req,res)=>{
     res.send("we are on POST")
 });
 
-db();
+
 
 const postRoute=require('./API/Post');
 app.use('/api/post',postRoute);
